153f07b44ee2a90078cdf43e768785a231202160
AlexDev <alexxspace@gmail.com>
2019-10-22 20:27:46 +0300

    step2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,6 +6,9 @@ const program = require('commander');
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((filepath1, filepath2) => {});
 
   program.parse(process.argv);
