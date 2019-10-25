797bcee3d6012a23ad12f1ffb5f0bd2712d30c6c
AlexDev <alexxspace@gmail.com>
2019-10-25 15:32:47 +0300

    step3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import path from 'path';
+import fs from 'fs';
 
-const genDiff = (d1, d2) => {
+const genDiff = (filePath1, filePath2) => {
 
-	const data1 = JSON.parse(d1, 'utf-8');
-	const data2 = JSON.parse(d2, 'utf-8');
+  const d1 = fs.readFileSync(filePath1, 'utf8');
+  const d2 = fs.readFileSync(filePath2, 'utf8');
+
+
+	const data1 = JSON.parse(d1);
+	const data2 = JSON.parse(d2,);
 
  	const keysFirst = Object.keys(data1);
  	const keysSecond = Object.keys(data2);
@@ -11,21 +16,21 @@ const genDiff = (d1, d2) => {
   const delKeys = keysFirst.filter((key) => !keysSecond.includes(key));
   const delData = delKeys.reduce((acc, elem) => {
     const str = data1[elem];
-    return [...acc, ` - ${elem}:${str}`];
+    return [...acc, `\n - ${elem}:${str}`];
   }, []);
 
  	const result = keysSecond.reduce((acc, elem) => {
  		if(keysFirst.includes(elem) && (data2[elem] !== data1[elem]) ) {
  				const str = data2[elem];
-         return [...acc, ` + ${elem}:${str}  - ${elem}:${data1[elem]}`];
+         return [...acc, ` + ${elem}:${str} \n - ${elem}:${data1[elem]}`];
  		}
     if(keysFirst.includes(elem) && (data2[elem] === data1[elem])) {
       const str = data2[elem];
-      return [...acc, ` ${elem}:${str}`];
+      return [...acc, `\n   ${elem}:${str}`];
     }
      if(!keysFirst.includes(elem)) {
       const str = data2[elem];
-      return [...acc, ` + ${elem}:${str}`];
+      return [...acc, `\n + ${elem}:${str}`];
     }
     
  		return acc;
