5873a43c2f7c9ff02ccfa9c99818341716d4f475
Alexander <48693252+durilka@users.noreply.github.com>
2019-11-05 00:50:16 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -16,21 +16,21 @@ const genDiff = (filePath1, filePath2) => {
   const delKeys = keysFirst.filter((key) => !keysSecond.includes(key));
   const delData = delKeys.reduce((acc, elem) => {
     const str = data1[elem];
-    return [...acc, ` - ${elem}:${str}`];
+    return [...acc, `- ${elem}:${str}`];
   }, []);
 
  	const result = keysSecond.reduce((acc, elem) => {
  		if(keysFirst.includes(elem) && (data2[elem] !== data1[elem]) ) {
  				const str = data2[elem];
-         return [...acc, ` + ${elem}:${str} \n - ${elem}:${data1[elem]}`];
+         return [...acc, `+ ${elem}:${str}\n- ${elem}:${data1[elem]}`];
  		}
     if(keysFirst.includes(elem) && (data2[elem] === data1[elem])) {
       const str = data2[elem];
-      return [...acc, `   ${elem}:${str}`];
+      return [...acc, `  ${elem}:${str}`];
     }
      if(!keysFirst.includes(elem)) {
       const str = data2[elem];
-      return [...acc, ` + ${elem}:${str}`];
+      return [...acc, `+ ${elem}:${str}`];
     }
     
  		return acc;
