08272cb3548af8e3f39274b79f8e267619b9fef0
AlexDev <alexxspace@gmail.com>
2019-11-09 00:22:52 +0300

    step5

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -5,6 +5,8 @@ const beforeJson = `${__dirname}/__fixtures__/before.json`;
 const afterJson = `${__dirname}/__fixtures__/after.json`;
 const beforeYaml = `${__dirname}/__fixtures__/before.yml`;
 const afterYaml = `${__dirname}/__fixtures__/after.yml`;
+const afterIni =`${__dirname}/__fixtures__/after.ini`;
+const beforeIni = `${__dirname}/__fixtures__/before.ini`;
 
 const diffBtoA = [
 	'+ timeout:20', 
@@ -15,6 +17,6 @@ const diffBtoA = [
 	'- follow:false',
 ].join('\n');
 
-test.each([[beforeJson, afterJson], [beforeYaml, afterYaml]])('current %#', (b, a) => {
+test.each([[beforeJson, afterJson], [beforeYaml, afterYaml], [beforeIni, afterIni]])('TEST %#', (b, a) => {
 	expect(gendiff(b, a)).toEqual(diffBtoA);
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const genDiff = (filePath1, filePath2) => {
 
@@ -13,10 +14,13 @@ let data2;
 if(path.extname(filePath1) === '.json' && path.extname(filePath2) === '.json') {
   data1 = JSON.parse(d1);
   data2 = JSON.parse(d2);
-} else if (path.extname(filePath1) === '.yml' && path.extname(filePath2) === '.yml') {
+} else if(path.extname(filePath1) === '.yml' && path.extname(filePath2) === '.yml') {
   data1 = yaml.safeLoad(d1);
   data2 = yaml.safeLoad(d2);
-}
+} else if(path.extname(filePath1) === '.ini' && path.extname(filePath2) === '.ini') {
+  data1 = ini.parse(d1);
+  data2 = ini.parse(d2);
+} 
 
  	const keysFirst = Object.keys(data1);
  	const keysSecond = Object.keys(data2);
