9fa40b64888622277eb998995a4976df4fe49586
AlexDev <alexxspace@gmail.com>
2019-12-23 16:15:40 +0300

    mentor correct

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,10 +4,10 @@ import gendiff from '../src';
 
 const getPathBefore = (fileFormat) => `${__dirname}/__fixtures__/before${fileFormat}`;
 const getPathAfter = (fileFormat) => `${__dirname}/__fixtures__/after${fileFormat}`;
-const getResultFile = (fileName) => fs.readFileSync(`${__dirname}/__fixtures__/${fileName}`, 'utf8');
+const getResultFile = (expectedFixture) => fs.readFileSync(`${__dirname}/__fixtures__/${expectedFixture}`, 'utf8');
 
 test.each`
-fileFormat 	| formatOut 	| fileName
+fileFormat 	| formatOut 	| expectedFixture
 ${'.ini'} 	| ${'plain'} 	| ${'plainResult.txt'}
 ${'.json'} 	| ${'nested'} | ${'nestedResult.txt'}
 ${'.yml'} 	| ${'json'} 	| ${'jsonResult.txt'}
@@ -15,8 +15,8 @@ ${'.yml'} 	| ${'json'} 	| ${'jsonResult.txt'}
 	const beforeData = getPathBefore(fileFormat);
 	const afterData = getPathAfter(fileFormat);
 
-	const receiveValue = gendiff(beforeData, afterData, formatOut);
-	const expectedValue = getResultFile(fileName);
+	const receivedValue = gendiff(beforeData, afterData, formatOut);
+	const expectedValue = getResultFile(expectedFixture);
 
 	expect(receiveValue).toBe(expectedValue);
 });
