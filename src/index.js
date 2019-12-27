dc2f46bdc1c17337f7372aa6978098c06ae10b67
AlexDev <alexxspace@gmail.com>
2019-12-27 23:35:23 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ export default (filePath1, filePath2, format) => {
 	const d1 = fs.readFileSync(filePath1, 'utf8');
 	const d2 = fs.readFileSync(filePath2, 'utf8');
 
-	const data1 = parse(path.extname(filePath1), d1);
-	const data2 = parse(path.extname(filePath2), d2);
+	const extensionD1 = path.extname(filePath1);
+	const extensionD2 = path.extname(filePath2);
+
+	const fileTypeD1 = extensionD1.slice(1);
+	const fileTypeD2 = extensionD2.slice(2);
+
+	const data1 = parse(fileTypeD1, d1);
+	const data2 = parse(fileTypeD2, d2);
 
 	const ast = astBuild(data1, data2);
 
